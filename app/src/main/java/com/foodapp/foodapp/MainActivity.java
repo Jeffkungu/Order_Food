08a1a375842fbc15d7976d9073e205d55980ae10
Jeff Kungu Gitenguri <jgitenguri@safaricom.co.ke>
2019-10-14 15:55:58 +0300

    [feature] adds changes

--- app/src/main/java/com/foodapp/foodapp/MainActivity.java
+++ app/src/main/java/com/foodapp/foodapp/MainActivity.java
@@ -2,12 +2,24 @@ package com.foodapp.foodapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonSignUp = findViewById(R.id.buttonSignUp);
+        Button buttonLogIn = findViewById(R.id.buttonLogIn);
+
+        buttonSignUp.setOnClickListener(this);
+        buttonLogIn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
